6c6118b30594f04c38979a064db8c3106ea4607f
unknown <lambonirinarufin@gmial.com>
2022-09-15 22:15:34 +0300

    modification

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
-});
-Route::get('/admin', function () {
-    return view('admin.dashboard');
+    return view('welcome');
 });
 
-
-Route::get('/dashboard', function () { 
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
