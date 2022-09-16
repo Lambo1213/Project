c025659dc47d320ece33350b1f7f3b8c6298ac14
unknown <lambonirinarufin@gmial.com>
2022-09-16 23:47:58 +0300

    ajout etudiant2

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('student', [StudentController::class, 'index'])->name('student.index');
-Route::get('student', [StudentController::class, 'store'])->name('student.store');
+Route::get('/student', [StudentController::class, 'index']);
+Route::post('/student', [StudentController::class, 'store']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 
 require __DIR__.'/auth.php';
